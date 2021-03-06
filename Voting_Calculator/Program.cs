e68cc20cd87e3d58c320b00e1c6dff42e493a431
Lukas-Trisauskas <Lukast4991@outlook.com>
2021-03-06 23:33:08 +0000

    Created a method that reads the file

--- Voting_Calculator/Program.cs
+++ Voting_Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Voting_Calculator
 {
@@ -7,6 +8,33 @@ namespace Voting_Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Development Branch");
+
+            // call the function and print the file inside the console
+
+            string data = ReadFile();
+
+            Console.WriteLine(data);
+        }
+        static string ReadFile()
+        {
+            // returns current working directory
+            
+            string currentDirectory = Directory.GetCurrentDirectory();
+
+            // combines two strings into a single path, currentDirectory + filename
+            // each [..\] means that it moves backup 1 directory from \..\bin\Debug\netcoreapp2.0\
+
+            string path = Path.Combine(currentDirectory, @"..\..\..\..\Assessment1Data.txt");
+
+            // returns actual path for the file
+
+            string filePath = Path.GetFullPath(path);
+
+            // takes file path as a an argument and reads the text file
+
+            string text = File.ReadAllText(filePath);
+
+            return text;
         }
     }
 }
