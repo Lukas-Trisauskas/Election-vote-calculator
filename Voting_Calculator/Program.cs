7076aa4d7db993351f1d603d57233469c2e580ff
Lukas-Trisauskas <Lukast4991@outlook.com>
2021-03-06 14:49:31 +0000

    Add project files.

---
+++ Voting_Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Voting_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
