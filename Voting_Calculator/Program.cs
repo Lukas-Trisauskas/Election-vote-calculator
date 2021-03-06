e60af05e6fea4454c1644085cae676f9e920d557
Lukas-Trisauskas <Lukast4991@outlook.com>
2021-03-06 20:58:57 +0000

    Initial test branch

--- Voting_Calculator/Program.cs
+++ Voting_Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Voting_Calculator
 {
@@ -6,7 +7,7 @@ namespace Voting_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Development Branch");
+            Console.WriteLine("Test Branch");
         }
     }
 }
