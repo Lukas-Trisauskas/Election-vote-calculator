5ea01b1d93660b3481bfa71a63c0e0fcbacdf8cd
George David Allbut <25419034@students.lincoln.ac.uk>
2021-03-06 15:29:08 +0000

    Initial dev branch

--- Voting_Calculator/Program.cs
+++ Voting_Calculator/Program.cs
@@ -6,7 +6,7 @@ namespace Voting_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Development Branch");
         }
     }
 }
