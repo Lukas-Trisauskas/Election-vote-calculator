76fab172e6177190175b12958457c750756110e5
Lukas-Trisauskas <Lukast4991@outlook.com>
2021-03-08 14:12:03 +0000

    Revert "Added a new class Calculator"
    This reverts commit 44b77acd17b4584fff8d22d3a6667a5c2539f96d.

--- Voting_Calculator/Program.cs
+++ Voting_Calculator/Program.cs
@@ -6,54 +6,44 @@ namespace Voting_Calculator
 {
     class Program
     {
-        private static List<Party> partyList = new List<Party>();
+        private static List<Party> parties = new List<Party>();
 
         static void Main(string[] args)
         {
-            /*
-             * Creates a new instance of a class, which takse 1 arg
-             * partyList is sent to the main class method [Calculator()]
-            */
-            Calculator newElection = new Calculator(partyList);
+            Console.WriteLine("Development Branch");
 
-        }
-        static void CreateParty()
-        {
-            // Calls the function and stores contents of the file inside a new variable
+            // call the function and print the file inside the console
             string data = ReadFile();
 
+            //Console.WriteLine(data);
 
-            // Splits each line as individual string and stores it in an array
-            String[] splitLines = data.Split(Char.Parse("\n"));
+            String[] lines = data.Split(Char.Parse("\n"));
 
-
-            /*
-             * [i = 3] means that it starts from the third line
-             * Each individual line is passed to main class method [Party()], which will seperate individual data inside that line
-             * E.g. Name, Votes, Seats etc.
-             * The seperated data is then added inside list object List<Party>
-            */
-            for (int i = 3; i < splitLines.Length; i++)
+            // For each line create and add a new Party to the list
+            for (int i = 3; i < lines.Length; i++)
             {
-                partyList.Add(new Party(splitLines[i]));
+                parties.Add(new Party(lines[i]));
             }
 
+            // Just testing and displaying all parties
+            foreach (Party party in parties)
+            {
+                Console.WriteLine($"{party.Name}:   Votes: {party.Votes}    Seats: {string.Join(", ", party.Seats)}");
+            }
         }
         static string ReadFile()
         {
-            // Returns current working directory
+            // returns current working directory
             string currentDirectory = Directory.GetCurrentDirectory();
 
-            /*
-             * Combines two strings into a single path, currentDirectory + filename
-             * Each [..\] means that it moves backup 1 directory from \..\bin\Debug\netcoreapp2.0\
-            */
+            // combines two strings into a single path, currentDirectory + filename
+            // each [..\] means that it moves backup 1 directory from \..\bin\Debug\netcoreapp2.0\
             string path = Path.Combine(currentDirectory, @"..\..\..\..\Assessment1Data.txt");
 
-            // Returns actual path for the file
+            // returns actual path for the file
             string filePath = Path.GetFullPath(path);
 
-            // Takes file path as an argument and reads the text file
+            // takes file path as an argument and reads the text file
             string text = File.ReadAllText(filePath);
 
             return text;
