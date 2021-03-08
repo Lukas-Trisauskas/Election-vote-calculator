7053b0ca793444d20f679c4e99c4e1a07fdd10de
Lukas-Trisauskas <Lukast4991@outlook.com>
2021-03-08 14:41:32 +0000

    Added method CreateParty

--- Voting_Calculator/Program.cs
+++ Voting_Calculator/Program.cs
@@ -6,44 +6,54 @@ namespace Voting_Calculator
 {
     class Program
     {
-        private static List<Party> parties = new List<Party>();
+        private static List<Party> partyList = new List<Party>();
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Development Branch");
+            /*
+             * Creates a new instance of a class, which takse 1 arg
+             * partyList is sent to the main class method [Calculator()]
+            */
+            Calculator newElection = new Calculator(partyList);
 
-            // call the function and print the file inside the console
+        }
+        static void CreateParty()
+        {
+            // Calls the function and stores contents of the file inside a new variable
             string data = ReadFile();
 
-            //Console.WriteLine(data);
 
-            String[] lines = data.Split(Char.Parse("\n"));
+            // Splits each line as individual string and stores it in an array
+            String[] splitLines = data.Split(Char.Parse("\n"));
 
-            // For each line create and add a new Party to the list
-            for (int i = 3; i < lines.Length; i++)
+
+            /*
+             * [i = 3] means that it starts from the third line
+             * Each individual line is passed to main class method [Party()], which will seperate individual data inside that line
+             * E.g. Name, Votes, Seats etc.
+             * The seperated data is then added inside list object List<Party>
+            */
+            for (int i = 3; i < splitLines.Length; i++)
             {
-                parties.Add(new Party(lines[i]));
+                partyList.Add(new Party(splitLines[i]));
             }
 
-            // Just testing and displaying all parties
-            foreach (Party party in parties)
-            {
-                Console.WriteLine($"{party.Name}:   Votes: {party.Votes}    Seats: {string.Join(", ", party.Seats)}");
-            }
         }
         static string ReadFile()
         {
-            // returns current working directory
+            // Returns current working directory
             string currentDirectory = Directory.GetCurrentDirectory();
 
-            // combines two strings into a single path, currentDirectory + filename
-            // each [..\] means that it moves backup 1 directory from \..\bin\Debug\netcoreapp2.0\
+            /*
+             * Combines two strings into a single path, currentDirectory + filename
+             * Each [..\] means that it moves backup 1 directory from \..\bin\Debug\netcoreapp2.0\
+            */
             string path = Path.Combine(currentDirectory, @"..\..\..\..\Assessment1Data.txt");
 
-            // returns actual path for the file
+            // Returns actual path for the file
             string filePath = Path.GetFullPath(path);
 
-            // takes file path as an argument and reads the text file
+            // Takes file path as an argument and reads the text file
             string text = File.ReadAllText(filePath);
 
             return text;
