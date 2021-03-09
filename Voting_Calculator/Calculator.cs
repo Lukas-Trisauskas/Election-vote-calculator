bf05a0fd5e45ad7549e3f8ecd932027b281b6ea8
Lukas-Trisauskas <Lukast4991@outlook.com>
2021-03-09 05:52:53 +0000

    Created while loop and implemented formula to workout the quotient at each round.

--- Voting_Calculator/Calculator.cs
+++ Voting_Calculator/Calculator.cs
@@ -1,21 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Voting_Calculator
 {
     class Calculator
     {
         // Properties
-        private int _round = 0;
-        private int _quotients = 0;
-        private int _seatsWon = 0;
+        private int _round = 1;
 
-        public Calculator(IEnumerable<Object> myList)
+        public Calculator(IEnumerable<Object> partyList)
         {
-            foreach (Party party in myList)
+
+            int seatsAllocated = 0;
+            while (seatsAllocated < 5)
             {
-                Console.WriteLine($"Party: {party.Name} | Votes: {party.TotalVotes} | Total Seats: {party.TotalSeats}");
+                Console.WriteLine($"Round: {_round}");
+                foreach (Party party in partyList)
+                {
+                    if (party.TotalSeats >= _round)
+                    {
+                        party.Quotient = party.TotalVotes / (seatsAllocated + 1);
+                        Console.WriteLine($"Party: {party.Name} | Votes: {party.TotalVotes} | Quotient: {party.Quotient}");
+                    }    
+                }
+                _round += 1;
+                seatsAllocated++;
             }
 
             /*
@@ -23,14 +34,13 @@ namespace Voting_Calculator
              * V = is total number of votes that party received
              * S = is the number of seats that party has been allocated so far, initially 0 for all parties
              * 
-             * The total voates for each part is divided, first by 1, and upto the total amount of seats that can be allocated -
+             * The total voates for each party is divided, first by 1, and upto the total amount of seats that can be allocated -
              * per party, which is 5. This gives us the quotient.
-             * At each round, the part whith highest quotient wins 1 seat.
+             * At each round, the party whith highest quotient wins 1 seat.
              * quotient = V / (S + 1)
            */
 
 
         }
-
     }
 }
